ca70d8b3206a2ef1172b93caa740cb13c3f9b41d
iamnikhilsutar <iamnikhilsutar@gmail.com>
2025-11-16 16:12:49 +0530

    Add HomeController

---
+++ src/main/java/com/github/nikhilsutarin/usermanagementsystem/controller/HomeController.java
@@ -0,0 +1,25 @@
+package com.github.nikhilsutarin.usermanagementsystem.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public ResponseEntity<Map<String, Object>> homeController(HttpServletRequest request) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("status", HttpStatus.OK.value());
+        body.put("error", "None");
+        body.put("message", "You are on home endpoint, see documentation for more information");
+        body.put("path", request.getServletPath());
+        return ResponseEntity.status(HttpStatus.OK).body(body);
+    }
+}
