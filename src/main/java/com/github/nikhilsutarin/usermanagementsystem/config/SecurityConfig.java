aadb9e6cd5ae5a6fee631f515e405e0d616964e7
iamnikhilsutar <iamnikhilsutar@gmail.com>
2025-11-18 13:38:38 +0530

    Update SecurityConfig

--- src/main/java/com/github/nikhilsutarin/usermanagementsystem/config/SecurityConfig.java
+++ src/main/java/com/github/nikhilsutarin/usermanagementsystem/config/SecurityConfig.java
@@ -90,6 +90,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/admin/**").hasRole("ADMIN")
                         .requestMatchers("/api/manager/**").hasRole("MANAGER")
                         .requestMatchers("/api/user/**").authenticated()
+                        .requestMatchers("/**").permitAll()
                         .anyRequest().authenticated())
                 .authenticationProvider(authenticationProvider())
                 .oauth2ResourceServer(oauth2 -> oauth2
